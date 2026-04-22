1ac22ab83b23db47afb1e5a25ad55d988f28791a
Svyatoslav Myshkovskiy <svatoslav190391@yandex.ru>
2026-04-22 21:26:41 +0300

    feat: реализовал асинхронный TCP-клиент с подключением и отключением

--- client/app/include/mainwindow.h
+++ client/app/include/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QAbstractSocket>
+#include <QHostAddress>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -10,19 +12,26 @@ class MainWindow;
 QT_END_NAMESPACE
 
 class QCloseEvent;
+class QTcpSocket;
 
 /*!
  * \class MainWindow
  * \brief Главное окно TCP-клиента для лабораторной работы №5.
  *
  * \details
- * На текущем этапе класс реализует базовую инициализацию интерфейса:
- * - значения по умолчанию для адреса, порта и timeout;
- * - настройку валидаторов для полей ввода;
- * - загрузку и сохранение пользовательских настроек через QSettings;
- * - стартовое состояние элементов управления.
+ * Класс реализует асинхронный TCP-клиент с графическим интерфейсом.
+ * Пользователь задаёт адрес и порт сервера, после чего по кнопке запускает
+ * подключение через QTcpSocket::connectToHost(). Отключение выполняется
+ * по запросу пользователя через QTcpSocket::disconnectFromHost().
  *
- * Сетевая логика на основе QTcpSocket будет добавлена следующим шагом.
+ * На текущем этапе реализованы:
+ * - ввод и валидация адреса, порта и timeout;
+ * - сохранение пользовательских настроек через QSettings;
+ * - асинхронное подключение и отключение;
+ * - обработка connected/disconnected/stateChanged/errorOccurred;
+ * - вывод изменений состояния сокета в qDebug().
+ *
+ * Кнопки write и stop пока зарезервированы под задание 3.
  */
 class MainWindow : public QMainWindow
 {
@@ -46,26 +55,52 @@ protected:
      * \param event Событие закрытия окна.
      *
      * \details
-     * Перед закрытием сохраняет текущие пользовательские настройки.
+     * Перед закрытием сохраняет настройки и корректно завершает соединение,
+     * если сокет ещё активен.
      */
     void closeEvent(QCloseEvent *event) override;
+
+private slots:
+    /*!
+     * \brief Обрабатывает нажатие кнопки подключения.
+     */
+    void onConnectClicked();
+
+    /*!
+     * \brief Обрабатывает нажатие кнопки отключения.
+     */
+    void onDisconnectClicked();
+
+    /*!
+     * \brief Обрабатывает успешное подключение к серверу.
+     */
+    void onSocketConnected();
+
+    /*!
+     * \brief Обрабатывает отключение от сервера.
+     */
+    void onSocketDisconnected();
+
+    /*!
+     * \brief Обрабатывает изменение состояния сокета.
+     * \param socketState Новое состояние сокета.
+     */
+    void onSocketStateChanged(QAbstractSocket::SocketState socketState);
+
+    /*!
+     * \brief Обрабатывает ошибку сокета.
+     * \param socketError Код ошибки сокета.
+     */
+    void onSocketErrorOccurred(QAbstractSocket::SocketError socketError);
 
 private:
     /*!
      * \brief Выполняет базовую инициализацию интерфейса.
-     *
-     * \details
-     * Метод:
-     * - задаёт значения по умолчанию;
-     * - настраивает валидаторы;
-     * - переводит лог в режим только для чтения;
-     * - устанавливает стартовое состояние кнопок;
-     * - загружает сохранённые настройки.
      */
     void initializeUi();
 
     /*!
-     * \brief Настраивает сигналы интерфейса.
+     * \brief Настраивает сигналы и слоты интерфейса и сокета.
      */
     void connectSignals();
 
@@ -79,8 +114,42 @@ private:
      */
     void saveSettings();
 
+    /*!
+     * \brief Добавляет строку в лог клиента.
+     * \param message Текст сообщения.
+     */
+    void appendLog(const QString &message);
+
+    /*!
+     * \brief Обновляет доступность элементов управления в зависимости от состояния сокета.
+     */
+    void updateConnectionControls();
+
+    /*!
+     * \brief Проверяет и разбирает параметры подключения из интерфейса.
+     * \param address Выходной адрес сервера.
+     * \param port Выходной порт сервера.
+     * \return true, если параметры корректны, иначе false.
+     */
+    bool tryGetConnectionParameters(QHostAddress &address, quint16 &port) const;
+
+    /*!
+     * \brief Возвращает строковое представление состояния сокета.
+     * \param socketState Состояние сокета.
+     * \return Строковое представление состояния сокета.
+     */
+    QString socketStateToString(QAbstractSocket::SocketState socketState) const;
+
+    /*!
+     * \brief Возвращает строковое представление ошибки сокета.
+     * \param socketError Код ошибки сокета.
+     * \return Строковое представление ошибки сокета.
+     */
+    QString socketErrorToString(QAbstractSocket::SocketError socketError) const;
+
 private:
     Ui::MainWindow *ui = nullptr;
+    QTcpSocket *socket_ = nullptr;
 };
 
 #endif // MAINWINDOW_H
--- client/app/src/mainwindow.cpp
+++ client/app/src/mainwindow.cpp
@@ -2,9 +2,17 @@
 #include "ui_mainwindow.h"
 
 #include <QCloseEvent>
+#include <QDateTime>
+#include <QLineEdit>
+#include <QMessageBox>
+#include <QPushButton>
+#include <QRadioButton>
 #include <QRegularExpression>
 #include <QRegularExpressionValidator>
 #include <QSettings>
+#include <QStatusBar>
+#include <QTcpSocket>
+#include <QDebug>
 
 namespace
 {
@@ -26,6 +34,7 @@ const QString kShortMode = QStringLiteral("short");
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , socket_(new QTcpSocket(this))
 {
     ui->setupUi(this);
     initializeUi();
@@ -44,6 +53,12 @@ MainWindow::~MainWindow()
 void MainWindow::closeEvent(QCloseEvent *event)
 {
     saveSettings();
+
+    if (socket_->state() != QAbstractSocket::UnconnectedState) {
+        socket_->disconnectFromHost();
+        socket_->close();
+    }
+
     QMainWindow::closeEvent(event);
 }
 
@@ -90,11 +105,10 @@ void MainWindow::initializeUi()
 
     ui->single_radioButton->setChecked(true);
 
-    ui->disconnect_pushButton->setEnabled(false);
-    ui->write_pushButton->setEnabled(false);
-    ui->stop_pushButton->setEnabled(false);
-
     loadSettings();
+    updateConnectionControls();
+
+    statusBar()->showMessage(QStringLiteral("Клиент не подключен"));
 }
 
 //--------------------------------------------------------------------------
@@ -118,6 +132,31 @@ void MainWindow::connectSignals()
 
     connect(ui->short_radioButton, &QRadioButton::toggled,
             this, &MainWindow::saveSettings);
+
+    connect(ui->connect_pushButton, &QPushButton::clicked,
+            this, &MainWindow::onConnectClicked);
+
+    connect(ui->disconnect_pushButton, &QPushButton::clicked,
+            this, &MainWindow::onDisconnectClicked);
+
+    connect(socket_, &QTcpSocket::connected,
+            this, &MainWindow::onSocketConnected);
+
+    connect(socket_, &QTcpSocket::disconnected,
+            this, &MainWindow::onSocketDisconnected);
+
+    connect(socket_, &QTcpSocket::stateChanged,
+            this, &MainWindow::onSocketStateChanged);
+
+#if QT_VERSION >= QT_VERSION_CHECK(5, 15, 0)
+    connect(socket_, &QTcpSocket::errorOccurred,
+            this, &MainWindow::onSocketErrorOccurred);
+#else
+    connect(socket_,
+            QOverload<QAbstractSocket::SocketError>::of(&QTcpSocket::error),
+            this,
+            &MainWindow::onSocketErrorOccurred);
+#endif
 }
 
 //--------------------------------------------------------------------------
@@ -191,4 +230,211 @@ void MainWindow::saveSettings()
     settings.setValue(kSessionModeKey, sessionMode);
 
     settings.endGroup();
+}
+
+//--------------------------------------------------------------------------
+
+void MainWindow::appendLog(const QString &message)
+{
+    const QString timestamp = QDateTime::currentDateTime().toString(QStringLiteral("HH:mm:ss"));
+    ui->clientLog_plainTextEdit->appendPlainText(
+        QStringLiteral("[%1] %2").arg(timestamp, message)
+    );
+}
+
+//--------------------------------------------------------------------------
+
+void MainWindow::updateConnectionControls()
+{
+    const QAbstractSocket::SocketState state = socket_->state();
+    const bool isConnected = state == QAbstractSocket::ConnectedState;
+    const bool isUnconnected = state == QAbstractSocket::UnconnectedState;
+
+    ui->connect_pushButton->setEnabled(isUnconnected);
+    ui->disconnect_pushButton->setEnabled(!isUnconnected);
+
+    ui->write_pushButton->setEnabled(false);
+    ui->stop_pushButton->setEnabled(false);
+
+    ui->address_lineEdit->setEnabled(isUnconnected);
+    ui->port_lineEdit->setEnabled(isUnconnected);
+
+    if (!isConnected) {
+        statusBar()->showMessage(QStringLiteral("Клиент не подключен"));
+    }
+}
+
+//--------------------------------------------------------------------------
+
+bool MainWindow::tryGetConnectionParameters(QHostAddress &address, quint16 &port) const
+{
+    if (!ui->address_lineEdit->hasAcceptableInput() ||
+        !ui->port_lineEdit->hasAcceptableInput()) {
+        return false;
+    }
+
+    const QString addressText = ui->address_lineEdit->text().trimmed();
+    const QString portText = ui->port_lineEdit->text().trimmed();
+
+    if (!address.setAddress(addressText)) {
+        return false;
+    }
+
+    bool isOk = false;
+    const uint parsedPort = portText.toUInt(&isOk);
+
+    if (!isOk || parsedPort == 0 || parsedPort > 65535) {
+        return false;
+    }
+
+    port = static_cast<quint16>(parsedPort);
+    return true;
+}
+
+//--------------------------------------------------------------------------
+
+QString MainWindow::socketStateToString(QAbstractSocket::SocketState socketState) const
+{
+    switch (socketState) {
+    case QAbstractSocket::UnconnectedState:
+        return QStringLiteral("UnconnectedState");
+    case QAbstractSocket::HostLookupState:
+        return QStringLiteral("HostLookupState");
+    case QAbstractSocket::ConnectingState:
+        return QStringLiteral("ConnectingState");
+    case QAbstractSocket::ConnectedState:
+        return QStringLiteral("ConnectedState");
+    case QAbstractSocket::BoundState:
+        return QStringLiteral("BoundState");
+    case QAbstractSocket::ListeningState:
+        return QStringLiteral("ListeningState");
+    case QAbstractSocket::ClosingState:
+        return QStringLiteral("ClosingState");
+    default:
+        return QStringLiteral("UnknownState");
+    }
+}
+
+//--------------------------------------------------------------------------
+
+QString MainWindow::socketErrorToString(QAbstractSocket::SocketError socketError) const
+{
+    switch (socketError) {
+    case QAbstractSocket::ConnectionRefusedError:
+        return QStringLiteral("ConnectionRefusedError");
+    case QAbstractSocket::RemoteHostClosedError:
+        return QStringLiteral("RemoteHostClosedError");
+    case QAbstractSocket::HostNotFoundError:
+        return QStringLiteral("HostNotFoundError");
+    case QAbstractSocket::SocketAccessError:
+        return QStringLiteral("SocketAccessError");
+    case QAbstractSocket::SocketResourceError:
+        return QStringLiteral("SocketResourceError");
+    case QAbstractSocket::SocketTimeoutError:
+        return QStringLiteral("SocketTimeoutError");
+    case QAbstractSocket::DatagramTooLargeError:
+        return QStringLiteral("DatagramTooLargeError");
+    case QAbstractSocket::NetworkError:
+        return QStringLiteral("NetworkError");
+    case QAbstractSocket::AddressInUseError:
+        return QStringLiteral("AddressInUseError");
+    case QAbstractSocket::SocketAddressNotAvailableError:
+        return QStringLiteral("SocketAddressNotAvailableError");
+    case QAbstractSocket::UnsupportedSocketOperationError:
+        return QStringLiteral("UnsupportedSocketOperationError");
+    default:
+        return QStringLiteral("UnknownSocketError");
+    }
+}
+
+//--------------------------------------------------------------------------
+
+void MainWindow::onConnectClicked()
+{
+    QHostAddress address;
+    quint16 port = 0;
+
+    if (!tryGetConnectionParameters(address, port)) {
+        QMessageBox::warning(
+            this,
+            QStringLiteral("Некорректные параметры"),
+            QStringLiteral("Введите корректные адрес и порт сервера.")
+        );
+        return;
+    }
+
+    saveSettings();
+
+    appendLog(QStringLiteral("Попытка подключения к %1:%2")
+              .arg(address.toString())
+              .arg(port));
+
+    statusBar()->showMessage(QStringLiteral("Подключение к серверу..."));
+
+    socket_->connectToHost(address, port);
+    updateConnectionControls();
+}
+
+//--------------------------------------------------------------------------
+
+void MainWindow::onDisconnectClicked()
+{
+    if (socket_->state() == QAbstractSocket::UnconnectedState) {
+        return;
+    }
+
+    appendLog(QStringLiteral("Запрошено отключение от сервера"));
+    socket_->disconnectFromHost();
+    updateConnectionControls();
+}
+
+//--------------------------------------------------------------------------
+
+void MainWindow::onSocketConnected()
+{
+    appendLog(QStringLiteral("Подключение к серверу установлено: %1:%2")
+              .arg(socket_->peerAddress().toString())
+              .arg(socket_->peerPort()));
+
+    statusBar()->showMessage(QStringLiteral("Клиент подключен к серверу"));
+    updateConnectionControls();
+
+    qDebug() << "CLIENT: connected to server"
+             << socket_->peerAddress().toString()
+             << socket_->peerPort();
+}
+
+//--------------------------------------------------------------------------
+
+void MainWindow::onSocketDisconnected()
+{
+    appendLog(QStringLiteral("Соединение с сервером закрыто"));
+    statusBar()->showMessage(QStringLiteral("Клиент отключен от сервера"));
+    updateConnectionControls();
+
+    qDebug() << "CLIENT: disconnected";
+}
+
+//--------------------------------------------------------------------------
+
+void MainWindow::onSocketStateChanged(QAbstractSocket::SocketState socketState)
+{
+    qDebug() << "CLIENT: socket state =" << socketStateToString(socketState);
+    updateConnectionControls();
+}
+
+//--------------------------------------------------------------------------
+
+void MainWindow::onSocketErrorOccurred(QAbstractSocket::SocketError socketError)
+{
+    const QString errorText = socketErrorToString(socketError);
+
+    qDebug() << "CLIENT: socket error =" << errorText
+             << "|" << socket_->errorString();
+
+    if (socketError != QAbstractSocket::RemoteHostClosedError) {
+        appendLog(QStringLiteral("Ошибка сокета: %1").arg(socket_->errorString()));
+    }
+
+    updateConnectionControls();
 }
